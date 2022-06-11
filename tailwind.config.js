b375ab1e01cf7350db7672157fcf89a59e591a04
Justin Scorzafava <jkscorzafava@gmail.com>
2022-06-11 12:41:11 -0600

    added barlow semi condensed as font and added images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./*.html"],
+	screens: {
+		sm: "480px",
+		md: "768px",
+		lg: "976px",
+		xl: "1440px",
+	},
+
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Barlow Semi Condensed", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
